5cf183888b6275eb87e4661e84126c0f3f3eab1d
augustus <augustus0818@gmail.com>
2018-08-02 13:34:38 +0800

    更新檔案

--- m-lazyload.js
+++ m-lazyload.js
@@ -1,29 +1,36 @@
-$(window).on('load', function() {
+$(window).on('load', () => {
   // 自由尺寸，先處理 padding-bottom
   if($('.w-free').length > 0) {
     $('.w-free').each(function() {
-      var w = $(this).find('.js-get-img').data('width');
-      var h = $(this).find('.js-get-img').data('height');
-      var pd = (h / w) * 100;
+      const w = $(this).find('.js-get-img').data('width'),
+            h = $(this).find('.js-get-img').data('height'),
+            pd = (h / w) * 100;
       $(this).find('.js-get-img').css('paddingBottom', pd + '%');
     });
   }
 
+
+  // 監聽 scroll，加載圖片
   $('.lazy-image').each(function() {
-    var self = $(this);
-    var waypoint = new Waypoint({
+    const self = $(this);
+    const waypoint = new Waypoint({
       element: self[0],
-      handler: function() {
-        var src = self.find('.js-get-img').data('img');
-        var img = new Image();
+      handler() {
+        const src = self.find('.js-get-img').data('img'); // 抓原始圖路徑
+
+        // 建立物件
+        const img = new Image();
             img.src = src;
         
-        $(img).on('load', function() {
+        // 圖片載入完，append 進 div 裡
+        $(img).on('load', () => {
           self.find('.js-get-img').append(img);
           setTimeout(function(){
             self.find('.js-get-img').addClass('show');
           }, 50);
         });
+
+        // destroy 該 div 的監聽 scroll
         this.destroy();
       },
       offset: '75%'
